5f479284b1282921135aa2d38499f4ab8ff40445
Yong Jun <yjthong.2009@smu.edu.sg>
2016-09-25 19:20:20 +0800

    Fixed typo in footer

--- server_src/server.dev.js
+++ server_src/server.dev.js
@@ -1,6 +1,6 @@
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
-import config from '../webpack.dev.config';
+import webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
+import config from '../webpack.dev.config'
 
 export default function () {
   let server = new WebpackDevServer(webpack(config), {
@@ -11,9 +11,9 @@ export default function () {
       '*': 'http://localhost:8080'
     },
     stats: { colors: true }
-  });
+  })
   server.listen(9000, '0.0.0.0', function (err, result) {
-    if (err) console.log(err);
-    console.log('Listening at 0.0.0.0:9000');
-  });
+    if (err) console.log(err)
+    console.log('Listening at 0.0.0.0:9000')
+  })
 }
